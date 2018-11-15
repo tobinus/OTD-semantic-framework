105b54fe8a01b5468199b062738c595b77d66f5d
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-11-15 10:49:21 +0100

    Implement CLI for starting webserver for search

---
+++ ontosearch/cli.py
@@ -0,0 +1,10 @@
+from os.path import dirname
+from utils.common_cli import make_subcommand_gunicorn
+
+
+def register_subcommand(add_parser):
+    parser = add_parser(
+        'serve',
+        help='Start the webserver for searching ontologies.',
+    )
+    make_subcommand_gunicorn(parser, dirname(__file__))
