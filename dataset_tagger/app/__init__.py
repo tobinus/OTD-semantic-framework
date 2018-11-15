292a4b76b698f6a0e83d4d4cb36d6183bc4dc386
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-11-15 12:05:31 +0100

    Explain why import is at bottom

--- dataset_tagger/app/__init__.py
+++ dataset_tagger/app/__init__.py
@@ -2,8 +2,10 @@ from os import path, environ
 from flask import Flask
 from dataset_tagger.config import Config
 
+# Set up app before trying to import routes that rely on it
 app = Flask(__name__)
 app.config.from_object(Config)
 app_path = path.dirname(__file__)
 
+# Set up routes
 from dataset_tagger.app import routes
