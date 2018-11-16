7c84d1880e2aab843a7d7d941ce23f981d3fc0c5
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-11-16 10:35:11 +0100

    Document utility functions for database interactions

--- utils/db.py
+++ utils/db.py
@@ -8,6 +8,21 @@ _has_loaded_dotenv = False
 
 
 def get_uri():
+    """
+    Obtain a default URI for connecting to MongoDB.
+
+    This uses the environment variables given to the application, as well as any
+    placed in .env files. The environment variables used are:
+    * DB_USERNAME
+    * DB_PASSWD
+    * DB_HOST
+    * DB_NAME (name of database to use)
+
+    Returns:
+        URI to be used with MongoClient's constructor, to connect to the
+        database configured by the user.
+
+    """
     global _cached_uri
 
     if _cached_uri is None:
@@ -17,6 +32,16 @@ def get_uri():
 
 
 def _create_uri_from_env():
+    """
+    Create a default URI using environment variables.
+
+    This function does the actual work, while get_uri() simply handles caching
+    of the created URI.
+
+    Returns:
+        URI to be used with MongoClient's constructor, to connect to the
+        database configured by the user.
+    """
     _ensure_loaded_dotenv()
 
     username = os.environ.get('DB_USERNAME', '')
@@ -28,6 +53,9 @@ def _create_uri_from_env():
 
 
 def _ensure_loaded_dotenv():
+    """
+    Ensure that any .env files have been loaded.
+    """
     global _has_loaded_dotenv
 
     if _has_loaded_dotenv:
@@ -47,6 +75,20 @@ def _ensure_loaded_dotenv():
 
 
 def create_uri(username, passwd, host, name):
+    """
+    Create a mongodb:// URI using the given details.
+
+    Args:
+        username: Username to authenticate with. Will be escaped.
+        passwd: Password to authenticate with. Will be escaped.
+        host: Host where the database can be found, optionally with a port.
+        name: Name of the database to connect to.
+
+    Returns:
+        URI to be used with MongoClient's constructor, to connect to the
+        database on the given host with the given username and password, using
+        the given database name.
+    """
     return 'mongodb://{}:{}@{}/{}'.format(
         urllib.parse.quote_plus(username),
         urllib.parse.quote_plus(passwd),
