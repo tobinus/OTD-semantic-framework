4d5841619187982c01d9d7f9de17f3061955e7d4
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-11-28 16:39:14 +0100

    Generalize CRUD commands for graphs

--- ontology/cli.py
+++ ontology/cli.py
@@ -1,36 +1,23 @@
-import argparse
-
-from bson.errors import InvalidId
-
 from ontology import SKOS_RDF_LOCATION
-from utils.common_cli import register_arguments_for_rdf_output, with_rdf_output
+from utils.common_cli import GraphSubcommand
 
 
-def register_subcommand(add_parser):
-    parser = add_parser(
-        'ontology',
-        help="Collection of helper commands for dealing with ontologies."
-    )
-    subcommands = parser.add_subparsers(
-        title="actions",
-        description="These actions are available to manipulate ontologies.",
-        dest="action"
-    )
-    register_generate(subcommands.add_parser)
-    register_create(subcommands.add_parser)
-    register_remove(subcommands.add_parser)
-    register_list(subcommands.add_parser)
-    register_show(subcommands.add_parser)
+def adjust_loaded_graph(self, g, args):
+    skos = args.skos
+    # Using SKOS.prefLabel as a substitute for checking for all SKOS subjects
+    has_skos = (self.utils_graph.SKOS.prefLabel, None, None) in g
+    if skos and not has_skos:
+        g.parse(skos)
+    elif not has_skos:
+        raise ValueError("The parsed graph does not contain SKOS triples and "
+                         "no SKOS location has been provided.")
 
 
-def register_generate(add_parser):
-    parser = add_parser(
-        'generate',
-        help="Create a serialization of the Open Transport Ontology.",
-        description="Create a serialization of the Open Transport Ontology."
-    )
-    register_arguments_for_rdf_output(parser)
-    parser.add_argument(
+def adjust_parsers(subparsers):
+    generate_parser = subparsers['generate']
+    create_parser = subparsers['create']
+
+    generate_parser.add_argument(
         '--skos',
         '-s',
         help="Embed triples defining SKOS in the serialization. You can "
@@ -40,155 +27,28 @@ def register_generate(add_parser):
         const=SKOS_RDF_LOCATION,
         default=False
     )
-    parser.set_defaults(
-        func=do_generate
-    )
 
-
-@with_rdf_output
-def do_generate(args):
-    from ontology.generate import create_graph
-    return create_graph(args.skos)
-
-
-def register_create(add_parser):
-    parser = add_parser(
-        'create',
-        help="Create new ontology in the database, using an ontology as the "
-             "basis. The UUID of the new ontology entry is printed.",
-        description="Create new ontology in the database, using an ontology as "
-                    "the basis. The UUID of the new ontology entry is printed."
-    )
-    parser.add_argument(
-        '--ontology',
-        '-o',
-        help="Location and format of ontology to load into the database. By "
-             "default, the authoritative version of the OTD ontology will be "
-             "used.",
-        nargs=2,
-        metavar=('LOCATION', 'FORMAT')
-    )
-    parser.add_argument(
-        '--uuid',
-        '-i',
-        help="Force the UUID for the new ontology, potentially overwriting an "
-             "existing ontology with the same UUID. If you do not provide a "
-             "UUID, it will be read from the ONTOLOGY_UUID environment "
-             "variable. When this flag is not given, a new UUID is generated "
-             "and printed.",
-        nargs='?',
-        const=None,
-        default=False
-    )
-    parser.add_argument(
+    create_parser.add_argument(
         '--skos',
         '-s',
         help="Location where SKOS RDF XML can be found. (Default: %(default)s)",
         default=SKOS_RDF_LOCATION
     )
-    parser.set_defaults(
-        func=do_create
-    )
 
 
-def do_create(args):
-    from ontology.create import insert_potentially_new_graph
-
-    if args.ontology:
-        ontology, ont_format = args.ontology
-    else:
-        ontology, ont_format = None, None
-
-    uuid = insert_potentially_new_graph(
-        args.uuid,
-        ontology,
-        ont_format,
-        args.skos
-    )
-    print(uuid)
+def do_generate(args):
+    from ontology.generate import create_graph
+    return create_graph(args.skos)
 
 
-def register_remove(add_parser):
-    help_text = (
-        "Remove the ontology with the specified UUID(s), no questions asked."
-    )
-    parser = add_parser(
-        'remove',
-        help=help_text,
-        description=help_text,
-    )
-    parser.add_argument(
-        'uuid',
-        help="UUID of ontology document(s) to remove",
-        nargs="+"
-    )
-    parser.set_defaults(
-        func=do_remove
-    )
+subcommand = GraphSubcommand(
+    'ontology',
+    'ontologies',
+    adjust_loaded_graph,
+    adjust_parsers,
+    do_generate
+)
 
 
-def do_remove(args):
-    from db import graph
-
-    success = True
-    for uuid in args.uuid:
-        try:
-            result = graph.remove(uuid, 'ontology')
-            if not result:
-                success = False
-                print('No document with UUID', uuid, 'found')
-        except InvalidId:
-            success = False
-            print('The UUID', uuid, 'is invalid')
-
-    return 0 if success else 1
-
-
-def register_list(add_parser):
-    help_text = (
-        "List all ontologies in the database."
-    )
-    parser = add_parser(
-        'list',
-        help=help_text,
-        description=help_text,
-    )
-    parser.set_defaults(
-        func=do_list
-    )
-
-
-def do_list(args):
-    from db import graph
-    ontologies = graph.find_all_ids('ontology')
-    for ontology in ontologies:
-        print(ontology)
-
-
-def register_show(add_parser):
-    help_text = (
-        "Display the contents of an ontology in the database."
-    )
-    parser = add_parser(
-        'show',
-        help=help_text,
-        description=help_text,
-    )
-    register_arguments_for_rdf_output(parser)
-    parser.add_argument(
-        'uuid',
-        help='The UUID of the ontology to show. If not given, then the UUID '
-             'named in the ONTOLOGY_UUID environment variable is used. If no '
-             'such variable is found, whatever MongoDB returns first is shown.',
-        nargs='?',
-        default=None
-    )
-    parser.set_defaults(
-        func=do_show
-    )
-
-
-@with_rdf_output
-def do_show(args):
-    from db import graph
-    return graph.get_ontology(args.uuid, False)
+def register_subcommand(add_parser):
+    return subcommand.register_subcommand(add_parser)
--- utils/common_cli.py
+++ utils/common_cli.py
@@ -94,3 +94,246 @@ def with_rdf_output(func):
             graph.serialize(args.out, format=args.format)
     return wrapped_function
 
+
+class GraphSubcommand:
+    SKOS_RDF_LOCATION = 'https://www.w3.org/2009/08/skos-reference/skos.rdf'
+
+    def __init__(self, key, plural, adjust_loaded_graph=None, adjust_parsers=None, graph_generate_func=None):
+        self.key = key
+        self.plural = plural
+        self.adjust_loaded_graph = adjust_loaded_graph
+        self.adjust_parsers = adjust_parsers
+        self.graph_generate_func = graph_generate_func
+        self.__db_graph = None
+        self.__utils_graph = None
+
+    @property
+    def db_graph(self):
+        if self.__db_graph is None:
+            from db import graph
+            self.__db_graph = graph
+        return self.__db_graph
+
+    @property
+    def utils_graph(self):
+        if self.__utils_graph is None:
+            from utils import graph
+            self.__utils_graph = graph
+        return self.__utils_graph
+
+    def insert_potentially_new_graph(self, uuid=False, location=None, format=None, args=None):
+        if uuid is not False:
+            return self.insert_graph(uuid, location, format, args)
+        else:
+            return self.insert_new_graph(location, format, args)
+
+    def insert_graph(self, uuid, location=None, format=None, args=None):
+        g = self.parse_graph_or_use_authoritative(location, format, args)
+        uuid = self.db_graph.get_uuid_for_collection(
+            self.key, uuid, True, 'insert_graph()'
+        )
+        self.db_graph.update(g, uuid, self.key)
+        return uuid
+
+    def insert_new_graph(self, location=None, format=None, args=None):
+        g = self.parse_graph_or_use_authoritative(location, format, args)
+        return self.db_graph.store(g, self.key)
+
+    def parse_graph_or_use_authoritative(self, location=None, format=None, args=None):
+        if location and format:
+            return self.parse_graph(location, format, args)
+        else:
+            if self.graph_generate_func:
+                return self.graph_generate_func(args)
+            else:
+                raise RuntimeError('Graph generating has not been implemented '
+                                   'for this subcommand, please provide either '
+                                   'function for generating graph or a graph '
+                                   'to load.')
+
+    def parse_graph(self, location, format, args):
+        g = self.utils_graph.create_bound_graph()
+        g.parse(location, format=format)
+
+        if self.adjust_loaded_graph:
+            g = self.adjust_loaded_graph(self, g, args)
+
+        return g
+
+    def register_subcommand(self, add_parser):
+        parser = add_parser(
+            self.key,
+            help=f"Collection of helper commands for dealing with {self.plural}."
+        )
+        subcommands = parser.add_subparsers(
+            title="actions",
+            description=f"These actions are available to manipulate {self.plural}.",
+            dest="action"
+        )
+        subparsers = dict()
+        if self.graph_generate_func:
+            subparsers['generate'] = self.register_generate(subcommands.add_parser)
+        subparsers['create'] = self.register_create(subcommands.add_parser)
+        subparsers['remove'] = self.register_remove(subcommands.add_parser)
+        subparsers['list'] = self.register_list(subcommands.add_parser)
+        subparsers['show'] = self.register_show(subcommands.add_parser)
+
+        if self.adjust_parsers:
+            self.adjust_parsers(subparsers)
+
+    def register_generate(self, add_parser):
+        parser = add_parser(
+            'generate',
+            help="Create a serialization of the Open Transport Ontology.",
+            description="Create a serialization of the Open Transport Ontology."
+        )
+        register_arguments_for_rdf_output(parser)
+        parser.set_defaults(
+            func=self.do_generate
+        )
+        return parser
+
+    def do_generate(self, args):
+        return with_rdf_output(self.graph_generate_func)(args)
+
+    def register_create(self, add_parser):
+        help_text = (
+            f"Create a new {self.key} graph in the database. The UUID of the new {self.key} entry is printed."
+        )
+        parser = add_parser(
+            'create',
+            help=help_text,
+            description=help_text,
+        )
+        if self.graph_generate_func:
+            parser.add_argument(
+                '--read',
+                '-r',
+                help=f"Location and format of the {self.key} graph to load into the database. By "
+                     f"default, an authoritative version will be "
+                     f"generated and used.",
+                nargs=2,
+                metavar=('LOCATION', 'FORMAT'),
+            )
+        else:
+            parser.add_argument(
+                'read',
+                help=f"Location and format of the {self.key} graph to load into the database.",
+                nargs=2,
+                metavar=('LOCATION', 'FORMAT'),
+            )
+        parser.add_argument(
+            '--uuid',
+            '-i',
+            help=f"Force the UUID for the new {self.key} graph, potentially overwriting an "
+                 f"existing {self.key} graph with the same UUID. If you do not provide a "
+                 f"UUID, it will be read from the {self.key.upper()}_UUID environment "
+                 f"variable. When this flag is not given, a new UUID is generated "
+                 f"and printed.",
+            nargs='?',
+            const=None,
+            default=False
+        )
+        parser.set_defaults(
+            func=self.do_create
+        )
+        return parser
+
+    def do_create(self, args):
+        if args.read:
+            location, rdf_format = args.read
+        else:
+            location, rdf_format = None, None
+
+        uuid = self.insert_potentially_new_graph(
+            args.uuid,
+            location,
+            rdf_format,
+            args
+        )
+        print(uuid)
+
+    def register_remove(self, add_parser):
+        help_text = (
+            f"Remove the {self.key} with the specified UUID(s), no questions asked."
+        )
+        parser = add_parser(
+            'remove',
+            help=help_text,
+            description=help_text,
+        )
+        parser.add_argument(
+            'uuid',
+            help=f"UUID of {self.key} document(s) to remove",
+            nargs="+"
+        )
+        parser.set_defaults(
+            func=self.do_remove
+        )
+        return parser
+
+    def do_remove(self, args):
+        from bson.errors import InvalidId
+        success = True
+        for uuid in args.uuid:
+            try:
+                result = self.db_graph.remove(uuid, self.key)
+                if not result:
+                    success = False
+                    print('No document with UUID', uuid, 'found')
+            except InvalidId:
+                success = False
+                print('The UUID', uuid, 'is invalid')
+
+        return 0 if success else 1
+
+    def register_list(self, add_parser):
+        help_text = (
+            f"List all {self.plural} in the database."
+        )
+        parser = add_parser(
+            'list',
+            help=help_text,
+            description=help_text,
+        )
+        parser.set_defaults(
+            func=self.do_list
+        )
+        return parser
+
+    def do_list(self, args):
+        documents = self.db_graph.find_all_ids(self.key)
+        for doc in documents:
+            print(doc)
+
+    def register_show(self, add_parser):
+        help_text = (
+            f"Display the contents of one row of {self.key} in the database."
+        )
+        parser = add_parser(
+            'show',
+            help=help_text,
+            description=help_text,
+        )
+        register_arguments_for_rdf_output(parser)
+        parser.add_argument(
+            'uuid',
+            help=f'The UUID of the {self.key} graph to show. If not given, then the UUID '
+                 f'named in the {self.key.upper()}_UUID environment variable is used. If no '
+                 f'such variable is found, whatever MongoDB returns first is shown.',
+            nargs='?',
+            default=None
+        )
+        parser.set_defaults(
+            func=self.do_show
+        )
+        return parser
+
+    def do_show(self, args):
+        # Work around with_rdf_output not made for methods
+        get_func = getattr(self.db_graph, f'get_{self.key}')
+
+        def call_get_func(args):
+            return get_func(args.uuid, False)
+
+        return with_rdf_output(call_get_func)(args)
