4471ecf9c04af9099c17af39fdd281b7363b7141
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-11-29 21:31:05 +0100

    Create full-fledged commands for similarity graphs

--- similarity/cli.py
+++ similarity/cli.py
@@ -1,20 +1,19 @@
-from utils.common_cli import register_arguments_for_rdf_output, with_rdf_output
+from utils.common_cli import GraphSubcommand
+
+
+def do_generate(args):
+    from similarity.generate import create_manual_tag_graph
+    return create_manual_tag_graph()
+
+
+subcommand = GraphSubcommand(
+    'similarity',
+    'similarities',
+    None,
+    None,
+    do_generate
+)
 
 
 def register_subcommand(add_parser):
-    help_text = "Generate RDF for manual tagging of datasets."
-    parser = add_parser(
-        'create_manual_tag',
-        help=help_text,
-        description=help_text,
-    )
-    register_arguments_for_rdf_output(parser)
-    parser.set_defaults(
-        func=do_subcommand
-    )
-
-
-@with_rdf_output
-def do_subcommand(args):
-    from similarity.generate import create_manual_tag_graph
-    return create_manual_tag_graph()
+    return subcommand.register_subcommand(add_parser)
