5f12ede5d8ffa4967576c793d9dddf68c98665fb
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-12-10 17:17:50 +0100

    Support filtering through package search, data.gov

--- dataset/generate.py
+++ dataset/generate.py
@@ -5,12 +5,10 @@ import requests
 
 
 def generate_dataset(ckan_url):
-    # Is this an API URL?
-    parsed_url = urlparse(ckan_url)
-    is_api_url = parsed_url.path.endswith('/action/package_search')
-
-    if is_api_url:
-        packages = get_datasets_from_package_search(full_url=ckan_url)
+    if is_package_search_url(ckan_url):
+        api_url = ckan_url
+        ckan_url = get_ckan_base_from_api_url(api_url)
+        packages = get_datasets_from_package_search(api_url)
     else:
         packages = get_datasets_from_package_list(ckan_url)
 
@@ -25,6 +23,29 @@ def generate_dataset(ckan_url):
     return graph
 
 
+def is_package_search_url(url):
+    # Strip off any query string and so on
+    parsed_url = urlparse(url)
+    return parsed_url.path.endswith('/action/package_search')
+
+
+def get_ckan_base_from_api_url(api_url):
+    pos = api_url.find('/api/')
+    # The base is everything up to /api/
+    return api_url[:pos]
+
+
+def get_datasets_from_package_search(api_url):
+    response = requests.get(api_url)
+    response.raise_for_status()
+
+    from_json = response.json()
+    result = from_json['result']
+    count = result['count']
+    package_detail_list = result['results']
+    return map(lambda details: details['name'], package_detail_list)
+
+
 def get_datasets_from_package_list(ckan_url):
     # Strip off any trailing slash, since we add slash ourselves
     if ckan_url[-1] == '/':
@@ -33,10 +54,10 @@ def get_datasets_from_package_list(ckan_url):
     # Download list of packages
     package_list = requests.get(
         f'{ckan_url}/api/3/action/package_list',
-
     )
-    # TODO: Handle data.gov redirecting to package_search
     package_list.raise_for_status()
+
+    # Were we redirected to package search? (Done by catalog.data.gov)
+    if is_package_search_url(package_list.url):
+        return get_datasets_from_package_search(package_list.url)
     return package_list.json()['result']
-
-
